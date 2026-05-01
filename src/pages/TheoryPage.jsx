5ba705fc87fc28d0c7a1d5031f54b1a267a98409
elleme1 <qaz49489@gmail.com>
2026-05-02 03:15:53 +0900

    feat: 자이브 개별 교육영상에 27번 로타리 지그재그 영상 추가

--- src/pages/TheoryPage.jsx
+++ src/pages/TheoryPage.jsx
@@ -495,6 +495,7 @@ export default function TheoryPage() {
               { id: 'u8MwDb0TfCo', num: 24, title: '카터펄트', eng: 'Catapult', desc: '투석기처럼 여성을 밀어 올리기 (현우·현아쌤)' },
               { id: '_Lcp0zPBhxY', num: 25, title: '숄더 스핀', eng: 'Shoulder Spin', desc: '어깨를 잡고 회전 (현우·현아쌤)' },
               { id: 'ak-oVgZmsck', num: 26, title: '처깅', eng: 'Chugging', desc: '기차처럼 씩씩하게 전진 (현우·현아쌤)' },
+              { id: 'R4Qf406MK8Y', num: 27, title: '로타리 지그재그', eng: 'Rotary Zigzag', desc: '회전하며 지그재그 이동 (현우·현아쌤)' },
 
             ].map(v => (
               <div
